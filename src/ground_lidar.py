78d3e635a395bdf1ab8a2a9bb3a3e106285c4ebd
Quadro-Del <antondel00@gmail.com>
2020-03-25 15:53:53 +0300

    bla

--- src/ground_lidar.py
+++ src/ground_lidar.py
@@ -8,12 +8,10 @@ Node for get altitude from seek lidar.
 import serial
 import rospy
 import tf
-import threading
 import time
 
-import numpy as np
 from std_msgs.msg import Float32
-from sensor_msgs.msg import LaserScan, Imu
+from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Quaternion
 
 show_plot_flag = False              # get plot
@@ -35,35 +33,12 @@ srez = 20
 lidar_offset = 0.0
 speed_change = 1.0
 
-use_filter_flag = True
-
 # init values
 current_val_lidar = 0.
 alt_msg = Float32()
-alt_array = list()
-offset_filter = 0.0
-init_filter = False
 rotate = Quaternion()
-srez_lidar = list()
 ser = None
 
-"""
-def filter(current_data, prev_data, current_speed, max_speed):
-    """
-    #Pub alt from lidar
-    #:param alt:
-    #:return:
-    """
-    global offset_filter
-    if current_speed < -max_speed:
-        print("jump UP")
-        offset_filter = prev_data - current_data
-        print(offset_filter)
-    if current_speed > max_speed:
-        print("jump DOWN")
-        offset_filter = 0.0
-        print(offset_filter)
-"""
 
 def getLidarData():
     """
@@ -72,7 +47,7 @@ def getLidarData():
     :return:
     """
 
-    global current_val_lidar, rotate, lidar_offset, srez_lidar, ser, use_filter_flag
+    global current_val_lidar, rotate, ser
     distance = 0.0
     _delay = 1./_rate_lidar
     try:
@@ -91,15 +66,46 @@ def getLidarData():
             euler = tf.transformations.euler_from_quaternion((rotate.x, rotate.y, rotate.z, rotate.w))
             dist_norm = (np.cos(euler[0]) * distance) * np.cos(euler[1])
 
-            current_val_lidar = dist_norm - lidar_offset
-
-            if use_filter_flag:
-                srez_lidar.append(current_val_lidar)
-                if len(srez_lidar) > srez:
-                    del srez_lidar[0]
+            current_val_lidar = dist_norm #- lidar_offset
 
             time.sleep(_delay)
     except:
         if ser != None:
             ser.close()
         exit()
+
+def callbackRot(data):
+    """
+    Callback IMU
+    :type data: Imu
+    :param data: data from tf
+    :return:
+    """
+    global rotate
+    rotate = data.orientation
+
+
+if __name__ == '__main__':
+
+    # Init node
+    rospy.init_node('lidar_seek_node', anonymous=True)
+
+    rate = rospy.Rate(_rate)        # set rate
+
+    # init params
+    port = rospy.get_param("~port", port)
+    # Subscriber
+    rospy.Subscriber(topic_imu, Imu, callbackRot)
+
+    # Publisher
+    alt_pub = rospy.Publisher(alt_topic, Float32, queue_size=10)
+
+    # init serial
+    ser = serial.Serial(port, 115200)
+    if ser.is_open == False:
+        ser.open()
+
+    #call getLidarData and output data on screen
+    while(True):
+        getLidarData()
+        print(current_val_lidar)
